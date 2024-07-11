d639a231e52a2b1c2f302821a5466b9039a86f2c
Arsenii <137112649+TymArsenii@users.noreply.github.com>
2024-07-11 12:54:58 +0300

    Update angle_processing.ino

--- firmware/angle_processing.ino
+++ firmware/angle_processing.ino
@@ -1,17 +1,37 @@
 void angle_pocessing()
 {
   uint16_t prd=255-mpu.getMag();
+  y_angle=constrain(prd, 0, 255);
   prd=constrain(prd, 15, 90);
-  y_angle=prd;
   if(mpu.update(prd))
   {
-    int angle_tmp=mpu.getAngle()+45;
-    if(angle_tmp>=0 || 1==1) angle=mpu.getAngle()+45;
+    
+    if(1==1) //1==2 for angle manually
+    {
+      int angle_tmp=mpu.getAngle()+45;
+      if(angle_tmp>=0 || 1==1) angle=mpu.getAngle()+45;
 
-    if (angle<0) angle+=360;
-    if (angle>=360) angle-=360;
+      if (angle<0) angle+=360;
+      if (angle>=360) angle-=360;
+    }
+
+    if(y_angle>=150)
+    {
+      if(sand_type=="water") rebuild_delay_time=10+10;
+      else if(sand_type=="normal") rebuild_delay_time=500+300;
+      else if(sand_type=="more_liquid") rebuild_delay_time=350+500;
+      else if(sand_type=="more_thick") rebuild_delay_time=1100+500;
+    }
+    else
+    {
+      if(sand_type=="water") rebuild_delay_time=10;
+      else if(sand_type=="normal") rebuild_delay_time=500;
+      else if(sand_type=="more_liquid") rebuild_delay_time=350;
+      else if(sand_type=="more_thick") rebuild_delay_time=1100;
+    }
     
     //Serial.print("angle: ");
     //Serial.println(prd);
+    Serial.println(angle);
   }
 }
