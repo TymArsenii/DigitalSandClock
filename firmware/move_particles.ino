2163c5273a5ca904abb87f8bf177f2dc2baf38cf
Arsenii <137112649+TymArsenii@users.noreply.github.com>
2024-09-03 21:58:04 +0300

    Update move_particles.ino

--- firmware/move_particles.ino
+++ firmware/move_particles.ino
@@ -1,5 +1,6 @@
 void move_particles()
 {
+  yield();
   // ============================================================== drop timers ==============================================================>
   
 
@@ -20,16 +21,16 @@ void move_particles()
   else if(sand_type=="more_thick") time_addend=((300*61)-9000)/1000;
   
   //time_addend+=1;
-  //Serial.println(time_addend);
+  //if(view_log) Serial.println(time_addend);
 
   float drop_timer_value=(float)(((minute*60)+second-time_addend)*1000/61);
-  Serial.println(drop_timer_value);
+  if(view_log) Serial.println(drop_timer_value);
 */
   if(!allow_drop)
   {
     if(angle>=180 && angle<=360)
     {
-      //Serial.println(matrix_arr[8-1][8-1]);
+      //if(view_log) Serial.println(matrix_arr[8-1][8-1]);
       if(matrix_arr[8-1][8-1]==0) matrix_arr[8-1][8-1]=2;
     }
     if(angle>=0 && angle<180)
@@ -60,7 +61,9 @@ void move_particles()
     {
       for(uint8_t y=1; y<=matrix_height; y++)
       {
+        //yield();
         delayMicroseconds(rebuild_delay_time);
+        //yield();
         move_timer=micros();
         if(matrix_arr[x-1][y-1]==1)
         {
@@ -178,7 +181,9 @@ void move_particles()
     {
       for(uint8_t x=1; x<=matrix_width; x++)
       {
+        //yield();
         delayMicroseconds(rebuild_delay_time);
+        //yield();
         move_timer=micros();
         if(matrix_arr[x-1][y-1]==1)
         {
@@ -226,13 +231,13 @@ void move_particles()
                 //last_positions_arr[last_elements_id][1]=y-1;
                 //if(last_elements_id<16) last_elements_id++;
 
-                //Serial.print(x);
-                //Serial.print(';');
-                //Serial.print(y);
-                //Serial.print(" -> ");
-                //Serial.print(x-1);
-                //Serial.print(';');
-                //Serial.print(y-1);
+                //if(view_log) Serial.print(x);
+                //if(view_log) Serial.print(';');
+                //if(view_log) Serial.print(y);
+                //if(view_log) Serial.print(" -> ");
+                //if(view_log) Serial.print(x-1);
+                //if(view_log) Serial.print(';');
+                //if(view_log) Serial.print(y-1);
               }
               if(x<matrix_width && y<matrix_height && matrix_arr[x][y]==0 && matrix_arr[x-1][y-1]==1) //left
               {
@@ -243,14 +248,14 @@ void move_particles()
                 //last_positions_arr[last_elements_id][1]=y-1;
                 //if(last_elements_id<16) last_elements_id++;
 
-                //Serial.print("   ");
-                //Serial.print(x);
-                //Serial.print(';');
-                //Serial.print(y);
-                //Serial.print(" -> ");
-                //Serial.print(x+1);
-                //Serial.print(';');
-                //Serial.print(y+1);
+                //if(view_log) Serial.print("   ");
+                //if(view_log) Serial.print(x);
+                //if(view_log) Serial.print(';');
+                //if(view_log) Serial.print(y);
+                //if(view_log) Serial.print(" -> ");
+                //if(view_log) Serial.print(x+1);
+                //if(view_log) Serial.print(';');
+                //if(view_log) Serial.print(y+1);
               }
             }
             else if(angle<=23)
@@ -283,10 +288,10 @@ void move_particles()
     {
       for(int x=1; x<=16; x++)
       {
-        //Serial.print(last_positions_arr[y-1][x-1]);
-        //Serial.print(" ");
+        //if(view_log) Serial.print(last_positions_arr[y-1][x-1]);
+        //if(view_log) Serial.print(" ");
       }
-      //Serial.println();
+      //if(view_log) Serial.println();
     }
   } //else if(angle>270 && angle<=360) 
   else if(angle>180 && angle<=270) //WORKS!
@@ -307,7 +312,9 @@ void move_particles()
     {
       for(uint8_t y=matrix_height; y>=1; y--)
       {
+        //yield();
         delayMicroseconds(rebuild_delay_time);
+        //yield();
         move_timer=micros();
         if(matrix_arr[x-1][y-1]==1)
         {
@@ -388,7 +395,9 @@ void move_particles()
     {
       for(uint8_t x=matrix_width; x>=1; x--)
       {
+        //yield();
         delayMicroseconds(rebuild_delay_time);
+        //yield();
         move_timer=micros();
         if(matrix_arr[x-1][y-1]==1)
         {
@@ -466,4 +475,69 @@ void move_particles()
   // <============================================================== moving!!! that WORKS!!! ==============================================================
 
   allow_drop=false;
+
+  bool empty=true;
+  if(angle>195 && angle<=250)
+  {
+    for(uint8_t x=9; x<=16; x++)
+    {
+      for(uint8_t y=9; y<=16; y++)
+      {
+        if(matrix_arr[x][y]==1) empty=false;
+      }
+    }
+
+    if(empty) 
+    {
+      beep_radar();
+      if(ee_data.angle_auto && curr_action==1)
+      {
+        if(millis()-angle_auto_timer>=angle_auto_period)
+        {
+          angle_auto_timer=millis();
+          angle_auto_period=random(1000, 6000);
+          angle=auto_angle_values[random(2)];
+        }
+      }
+      //Serial.println("BEEP!");
+    }
+    else 
+    {
+      beep_stop_timer=millis();
+      digitalWrite(15, 0);
+    }
+  }
+  else if(angle>15 && angle<=75)
+  {
+    for(uint8_t x=1; x<=8; x++)
+    {
+      for(uint8_t y=1; y<=8; y++)
+      {
+        if(matrix_arr[x][y]==1) empty=false;
+      }
+    }
+
+    if(empty) 
+    {
+      beep_radar();
+
+      if(ee_data.angle_auto && curr_action==1)
+      {
+        if(millis()-angle_auto_timer>=angle_auto_period)
+        {
+          angle_auto_timer=millis();
+          angle_auto_period=random(1000, 6000);
+          angle=auto_angle_values[random(2)];
+        }
+      }
+      
+      //Serial.println("BEEP!");
+    }
+    else 
+    {
+      beep_stop_timer=millis();
+      digitalWrite(15, 0);
+    }
+  }
+  yield();
 }
